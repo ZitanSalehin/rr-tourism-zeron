f7ec26a55c67b49a3f3cbaabfccda97324c7c885
ZitanSalehin <zitansalehin456@gmail.com>
2025-12-09 01:51:31 +0600

    test

--- src/pages/blog/BlogContent.jsx
+++ src/pages/blog/BlogContent.jsx
@@ -48,7 +48,6 @@ export default function BlogContent() {
             key={post.id}
             className="bg-white h-fit rounded-xl shadow-md overflow-hidden"
           >
-            {/* Image Wrapper */}
             <div className="relative overflow-hidden">
               <img
                 src={post.img}
